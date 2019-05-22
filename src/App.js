ba4888e725123a773a16d14a6ffc63d011b5497b
bogus1aw <boguslaw.mail@gmail.com>
2019-05-22 10:12:49 +0200

    connect to baskend and test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+import {client} from './index'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GET_REPOSITORIES_OF_ORGANIZATION = gql `
+{user(id: 7){
+  id
+  name
+}}
+`;
+
+const App = () => (
+  <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
+    {({loading, error, data}) => {
+      if (loading) 
+        return <div>Fetching</div>
+      if (error) 
+        return <div>Error</div>
+
+      return (
+        <div>
+          {/* {linksToRender.map(link => <Link key={link.id} link={link} />)} */
+          console.log("test")}
+          {"test"}
+          <br/> {JSON.stringify(data)}
+          <br/> {data.user.id}
+          <br/> {JSON.stringify("getAllTasks(): " + getAllTasks())
+}
+        </div>
+      )
+    }}
+  </Query>
+
+);
+
+export async function getAllTasks() {
+  let taskList;
+  await client
+    .query({query: gql `
+      {user(id: 6){
+        id
+        name
+      }}
+      `})
+    .then(response => {
+      taskList = response.data;
+      console.log("response.data: " + JSON.stringify(response.data))
+
+    });
+  console.log("JSON.stringify(taskList)")
+  console.log(JSON.stringify(taskList))
+  return taskList;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import App from './App';
+// import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { AUTH_TOKEN } from './constants'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+
+const BACKEND_BASE_URL = 'http://localhost:8080/graphql';
+
+localStorage.setItem(AUTH_TOKEN, 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTU4NDc5OTY5LCJleHAiOjE1NTkwODQ3Njl9.zy3GR3dJ_B3f5tDT8LYSYRplh4Kxdae_TrYXlLDPd0iRWwdvOnHVvsuRonzqRO4OD3c4-YT7q_leFh50WFfU7A')
+const token = localStorage.getItem(AUTH_TOKEN)
+
+const httpLink = new HttpLink({
+    uri: BACKEND_BASE_URL,
+    headers: {
+        authorization: `Bearer ${token}`
+        // authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTU4NDc5OTY5LCJleHAiOjE1NTkwODQ3Njl9.zy3GR3dJ_B3f5tDT8LYSYRplh4Kxdae_TrYXlLDPd0iRWwdvOnHVvsuRonzqRO4OD3c4-YT7q_leFh50WFfU7A`
+    }
+});
+
+export const client = new ApolloClient({link: httpLink, cache});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+    <div>
+        {"test1"}
+    </div>
+    <App/>
+</ApolloProvider>, document.getElementById('root'),);
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// unregister() to register() below. Note this comes with some pitfalls. Learn
+// more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
